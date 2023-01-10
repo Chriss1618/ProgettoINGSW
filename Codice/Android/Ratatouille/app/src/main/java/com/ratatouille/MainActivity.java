a4a5546fbb4352174fea994556a3fefe192c123f
Chriss_PcFisso <basketcristian@gmail.com>
2023-01-10 18:44:11 +0100

    [Codice Android] Testing Standards

--- Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/MainActivity.java
+++ Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/MainActivity.java
@@ -3,7 +3,13 @@ package com.ratatouille;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //SYSTEM
@@ -11,17 +17,55 @@ public class MainActivity extends AppCompatActivity {
 
     //LAYOUT
     Button Button_Login;
-
+    TextView TextView_Testo;
     //FUNCTIONAL
 
-
     //OTHER...
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PrepareData();
+
+        PrepareLayout();
+
+
+
+        Log.d(TAG, "onCreate: Hai creato la schermata");
     }
+
+
+
+    private void PrepareData() {
+
+    }
+
+    private void PrepareLayout() {
+        LinkLayout();
+        SetDataOnLayout();
+        SetActionsOfLayout();
+    }
+
+    private void LinkLayout() {
+        Button_Login    =       findViewById(R.id.button_login);
+        TextView_Testo  =       findViewById(R.id.text_view_testo);
+    }
+
+    private void SetDataOnLayout() {
+        TextView_Testo.setText(messaggio);
+    }
+
+    private void SetActionsOfLayout() {
+        Button_Login.setOnClickListener(View ->onButtonLogin());
+    }
+
+    //BUTTONS ACTIONS
+    private void onButtonLogin(){
+
+    }
+
+
 
 }
