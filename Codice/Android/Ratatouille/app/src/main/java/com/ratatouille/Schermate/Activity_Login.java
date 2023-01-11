fc9d362f6f992eff9f467c4f420457f8f5a8173b
Chriss_PcFisso <basketcristian@gmail.com>
2023-01-11 11:01:19 +0100

    [Codice Android] Modifica Activity Login

--- Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/Schermate/Activity_Login.java
+++ Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/Schermate/Activity_Login.java
@@ -13,7 +13,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_Login extends AppCompatActivity {
     //SYSTEM
     private static final String TAG = "MainActivity";
 
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
         PrepareLayout();
 
-
-
         Log.d(TAG, "onCreate: Hai creato la schermata");
     }
 
@@ -51,15 +49,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void LinkLayout() {
-        Button_Login    =       findViewById(R.id.button_login);
-        TextView_Testo  =       findViewById(R.id.text_view_testo);
+        //Button_Login    =       findViewById(R.id.button_login);
+        //TextView_Testo  =       findViewById(R.id.text_view_testo);
     }
 
     private void SetDataOnLayout() {
     }
 
     private void SetActionsOfLayout() {
-        Button_Login.setOnClickListener(View ->onButtonLogin());
+        //Button_Login.setOnClickListener(View ->onButtonLogin());
     }
 
     //BUTTONS ACTIONS
