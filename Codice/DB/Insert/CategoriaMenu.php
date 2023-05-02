86d8ba974890d2bf1263913272a7376c544ff2c4
chris_laptop <basketcristian@gmail.com>
2023-05-02 22:12:47 +0200

    [ADD] Codice BackEnd : - aggiunto protezione da SQL injection

--- Codice/DB/Insert/CategoriaMenu.php
+++ Codice/DB/Insert/CategoriaMenu.php
@@ -1,41 +1,37 @@
 <?php
     include 'functions.php';
     include '../../db_connection.php';
-    
 
     $con = new mysqli($host_name, $user_name, $password, $database);
-   
-
+    header('Content-Type: application/json; charset=utf-8');
     if ($con == false) {
         $arrayJson['status'] = 0;
         $arrayJson['msg'] = " Collegamento al server MySQL non riuscito ";
     }else {
-        $Id_Ristorante = $con->real_escape_string($_POST['id_ristorante']);
+        $Id_Ristorante = $_POST['id_ristorante'];
+        //DEFENDING against sql injection using real escape string 
+        //sostituisce tutti caratteri speciali che possono compromettere il DB
         $NameCategory = $con->real_escape_string($_POST['NameCategory']);
 
         $insert = "INSERT INTO `CategoriaMenu`( `ID_Ristorante`, `NameCategory`) 
-        VALUES ($Id_Ristorante,'$NameCategory'); ";
-        
-        if( $conn->query($insert) == true ){
-            $id_categoria = $conn->insert_id;
-
+        VALUES ($Id_Ristorante,'$NameCategory') ";
+       
+        if( $con->query($insert) == true ){
             $arrayJson['status'] = 1;
+            $id_newCategory = $con->insert_id;
+            $messageArray = [];
             $message = array (
-                'NomeCategoria' => $row['NameCategory'],
-                'ID_CategoriaMenu' => $row['ID_CategoryMenu']
+                'NomeCategoria' => $NameCategory,
+                'ID_CategoriaMenu' => $id_newCategory
             );
             array_push($messageArray, $message);
             $arrayJson['msg'] = $messageArray;
         }else{
             $arrayJson['status'] = 0;
-            $arrayJson['msg'] = " errore ";
-            // $arrayJson['msg'] = $conn->error;
+            $arrayJson['msg'] = $con->error;
         }
-
-        
-
         $con->close();
     }
-    header('Content-Type: application/json; charset=utf-8');
+    
     echo json_encode($arrayJson);
 ?>
