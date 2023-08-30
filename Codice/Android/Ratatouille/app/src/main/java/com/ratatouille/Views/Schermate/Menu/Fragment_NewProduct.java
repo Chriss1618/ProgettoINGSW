ebbcc140a995e04b7deec88e5afef6daa16af0af
chris_laptop <basketcristian@gmail.com>
2023-08-30 15:11:47 +0200

    [TO DO] Vincolare la selezione all'unita di misura tra Grammi e Litri in base al tipo dell'ingrediente seelto da aggiungere al prodotto

--- Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/Views/Schermate/Menu/Fragment_NewProduct.java
+++ Codice/Android/Ratatouille/app/src/main/java/com/ratatouille/Views/Schermate/Menu/Fragment_NewProduct.java
@@ -81,6 +81,7 @@ public class Fragment_NewProduct extends Fragment implements ViewLayout {
             Categoria = (CategoriaMenu) manager.getData();
             PrepareReceiveFromGallery();
         }
+
     }
 
     @Override
